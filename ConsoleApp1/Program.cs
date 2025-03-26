b81f879ec04a2033da91b1963fe41282cad3762e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-03-26 14:52:40 +0000

    Print NumberFormatInfo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Globalization;
+using System.Text.Json;
 
 var cultureInfo = new CultureInfo("de-AT");
 
@@ -8,6 +9,7 @@ Thread.CurrentThread.CurrentUICulture = cultureInfo;
 
 try
 {
+    Console.WriteLine(JsonSerializer.Serialize(NumberFormatInfo.GetInstance(cultureInfo), new JsonSerializerOptions { WriteIndented = true }));
     _ = double.Parse("3.5");
     throw new Exception("Format exception not raised.");
 }
